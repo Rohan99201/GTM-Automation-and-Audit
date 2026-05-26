ddbcf343f13738a4a45d7dacc70654a3c8490906
Rohan99201 <rohanchoubey04@gmail.com>
2026-05-26 17:11:41 +0530

    fix: move authOptions out of route file to satisfy Next.js types

--- app/api/gtm/route.js
+++ app/api/gtm/route.js
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "../auth/[...nextauth]/route";
+import { authOptions } from "../../../lib/authOptions";
 import * as GTM from "../../../lib/gtm";
 
 export async function GET(req) {
